568f68fb98485bd2518de9ba73c51d00e62c764a
SukramJ <sukramj@icloud.com>
2025-10-02 15:04:27 +0200

    Update version

--- mtec2mqtt/__init__.py
+++ mtec2mqtt/__init__.py
@@ -8,4 +8,4 @@ Modbus, optional Home Assistant discovery integration, and small utilities.
 from __future__ import annotations
 
 __all__ = ["config", "mqtt_client", "hass_int", "modbus_client"]
-__version__ = "2.0.4"
+__version__ = "2.0.5"
